9a4ff68b742bf76caa9d761a116df8ddec2f425f
ydh0428 <133204393+ydh0428@users.noreply.github.com>
2023-06-15 12:04:59 +0000

    6-15

--- sticky_memo/memo/views.py
+++ sticky_memo/memo/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from memo.models import Memo
 def get_all_memo(request):
     memo_list = Memo.objects.all()
     context = {"memos": memo_list}
 
     return render(request, "memo_list.html", context)
+
+def get_memo(request, pk):
+    memo = Memo.objects.get(id=pk)
+    context = {"memo": memo}
+
+    return render(request, "memo_detail.html", context)
+
+from memo.forms import MemoForm
+def create_memo(request):
+    if request.method == 'GET':
+        form = MemoForm()
+        context = {'form': form}
+        return render(request, 'memo_create.html', context)
+
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
